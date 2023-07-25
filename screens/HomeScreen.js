c71303d8b7163d193a9e93e0b2588603405f7e50
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-25 13:31:40 -0400

    HomeScreen Design

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,44 @@
 import React from 'react'
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, Platform } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Bars3CenterLeftIcon } from 'react-native-heroicons/outline'
+
+const ios = Platform.OS == 'ios'
 
 const HomeScreen = () => {
+    const { container, safeArea, icons, logo } = styles
     return (
-        <SafeAreaView>
-            <Text>HomeScreen</Text>
-        </SafeAreaView>
+        <View style={container}>
+            <SafeAreaView style={ ios ? safeArea : marginBottom=3 }>
+                <StatusBar style='light'/>
+                <View style={icons}>
+                    <Bars3CenterLeftIcon size={30} strokeWidth={2} color={'white'} />
+                    <Text style={logo}>Movies</Text>
+                </View>
+            </SafeAreaView>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: '#262626'
+    },
+    safeArea: {
+        marginBottom: -2
+    },
+    icons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: 4
+    },
+    logo: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
 })
 
 export default HomeScreen
