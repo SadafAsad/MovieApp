71945ea914ccdb79e5a0384f9515a743b354c094
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-25 14:51:37 -0400

    Testing Carousel

---
+++ screens/components/MovieCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { TouchableWithoutFeedback, StyleSheet, Text } from 'react-native'
+
+const MovieCard = ({ item }) => {
+    <TouchableWithoutFeedback>
+        <Text>Movie</Text>
+    </TouchableWithoutFeedback>
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'white',
+        fontSize: 20
+    }
+})
+
+export default MovieCard
--- screens/components/TrendingMovies.js
+++ screens/components/TrendingMovies.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, TouchableWithoutFeedbackComponent } from 'react-native'
+import { MoonIcon } from 'react-native-heroicons/outline'
+import Carousel from 'react-native-snap-carousel'
+import MovieCard from './MovieCard'
 
 const TrendingMovies = ({ data }) => {
     const { container, textDesign } = styles
     return (
         <View style={container}>
             <Text style={textDesign}>Trending</Text>
+            <Carousel 
+                data={data}
+                renderItem={({item}) => <MovieCard item={item} />}
+                firstItem={1}
+                inactiveSlideOpacity={0.60}
+                sliderWidth={600}
+                itemWidth={400}
+                slideStyle={{display: 'flex', alignItems: 'center'}}
+            />
         </View>
     )
 }
