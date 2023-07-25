f0ae247f84b28f5c9f0d8249c0c4f595e629af31
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-25 14:20:39 -0400

    scroll view for trending movies added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,31 @@
 import React from 'react'
-import { View, Text, SafeAreaView, StyleSheet, Platform, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, Platform, TouchableOpacity, ScrollView } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
 
 const ios = Platform.OS == 'ios'
 
 const HomeScreen = () => {
-    const { container, safeArea, icons, logo } = styles
+    const { container, safeArea, icons, logo, mDesign } = styles
     return (
         <View style={container}>
             <SafeAreaView style={ ios ? safeArea : marginBottom=3 }>
                 <StatusBar style='light'/>
                 <View style={icons}>
                     <Bars3CenterLeftIcon size={30} strokeWidth={2} color={'white'} />
-                    <Text style={logo}>Movies</Text>
+                    <Text style={logo}>
+                        <Text style={mDesign}>M</Text>ovies</Text>
                     <TouchableOpacity>
                         <MagnifyingGlassIcon size={30} strokeWidth={2} color={'white'} />
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{paddingBottom: 10}}
+            >
+                
+            </ScrollView>
         </View>
     )
 }
@@ -39,8 +46,11 @@ const styles = StyleSheet.create({
     },
     logo: {
         color: 'white',
-        fontSize: 20,
+        fontSize: 25,
         fontWeight: 'bold'
+    },
+    mDesign: {
+        color: '#eab308'
     }
 })
 
