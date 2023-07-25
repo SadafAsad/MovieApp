fea379837c6e3f390eb4b0140729b6677accc4c5
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-25 12:23:07 -0400

    HomeScreen added

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>HomeScreen</Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default HomeScreen
