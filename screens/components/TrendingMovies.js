68dc9ecd638a074213db64939b3f3dd4c82a3c36
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-25 14:22:52 -0400

    TendingMovies component added

---
+++ screens/components/TrendingMovies.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+const TrendingMovies = () => {
+    return (
+        <View>
+            <Text>Trending Movies</Text>
+        </View>
+    )
+}
+
+export default TrendingMovies
