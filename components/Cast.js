ca506c8f93ac07f50d19f07e3651a88cba561141
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-19 11:19:50 -0400

    Cast component added

---
+++ components/Cast.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { StyleSheet, View, Text } from "react-native"
+
+const Cast = () => {
+    return (
+        <View>
+            <Text>Cast</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Cast
