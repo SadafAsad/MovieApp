4b5a013b06563b9c5f1c3971d55397ad4073528e
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-19 08:44:22 -0400

    Movie Screen partially added

---
+++ screens/MovieScreen.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { StyleSheet, View } from "react-native"
+
+const MovieScreen = () => {
+    return (
+        <View></View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default MovieScreen
