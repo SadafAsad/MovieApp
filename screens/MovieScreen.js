05e06991ee10125695219e69b8d58a0ba0bfe6d9
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-19 09:50:07 -0400

    working on movie screen

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,11 +1,37 @@
-import React from "react"
-import { StyleSheet, View, Text } from "react-native"
+import React, { useEffect } from "react"
+import { StyleSheet, View, Text, ScrollView, SafeAreaView, TouchableOpacity } from "react-native"
+import { useRoute } from "@react-navigation/native"
+import { ChevronLeftIcon } from "react-native-heroicons/outline"
 
 const MovieScreen = () => {
+    const {params: item} = useRoute()
+
+    useEffect(() => {
+
+    }, [item])
+
     return (
-        <View>
-            <Text>Movie Screen</Text>
-        </View>
+        <ScrollView
+            contentContainerStyle={{paddingBottom: 20}}
+            style={{flex: 1, backgroundColor: '#262626'}}
+        >
+            <View>
+                <SafeAreaView
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        paddingHorizontal: 4
+                    }}
+                >
+                    <TouchableOpacity
+                        style={{borderRadius: 10, padding: 1, backgroundColor: '#eab308'}}
+                    >
+                        <ChevronLeftIcon size='28' strokeWidth={2.5} color='white' />
+                    </TouchableOpacity>
+                </SafeAreaView>
+            </View>
+        </ScrollView>
     )
 }
 
