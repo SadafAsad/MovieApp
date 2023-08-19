09337a7f55b899b296bd9de832a6ff152548f5d1
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-19 08:40:43 -0400

    upcoming movies view added

--- components/MovieList.js
+++ components/MovieList.js
@@ -1,10 +1,47 @@
 import React from "react"
-import { View, StyleSheet, Text } from "react-native"
+import { View, StyleSheet, Text, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Image, Dimensions } from "react-native"
+import { useNavigation } from "@react-navigation/native"
 
-const MovieList = () => {
+var {width, height} = Dimensions.get('window')
+
+const MovieList = ({ title, data }) => {
+    let movieName = "Oppenheimer"
+    const navigation = useNavigation()
+
     return ( 
         <View> 
-            <Text>MovieList</Text>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                <Text style={{color: 'white', fontSize: 20}}>{title}</Text>
+                <TouchableOpacity>
+                    <Text style={{color: '#eab308'}}>See All</Text>
+                </TouchableOpacity>
+            </View>
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{paddingHorizontal: 15}}
+            >
+                {
+                    data.map((item, index) => {
+                        return (
+                            <TouchableWithoutFeedback
+                                key={index}
+                                onpress={() => navigation.navigate('Movie', item)}
+                            >
+                                <View style={{margin: 4}}>
+                                    <Image 
+                                        source={require('../assets/oppenheimer.jpg')}
+                                        style={{width: width*0.33, height: height*0.22, borderRadius: 10}}
+                                    />
+                                    <Text style={{color: 'white'}}>{
+                                        movieName.length>14 ? movieName.slice(0,14)+'...' : movieName
+                                    }</Text>
+                                </View>
+                            </TouchableWithoutFeedback>
+                        )
+                    })
+                }
+            </ScrollView>
         </View>
     )
 }
