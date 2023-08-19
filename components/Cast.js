f47841a14e2a9e220786b5bf576d4d67b8fd4da7
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-19 11:39:50 -0400

    Adding cats to the movie screen

--- components/Cast.js
+++ components/Cast.js
@@ -1,10 +1,55 @@
 import React from "react"
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity, Image } from "react-native"
 
-const Cast = () => {
+const Cast = ({ cast, navigation }) => {
+    let personName = 'Margo Robbie'
+    let characterName = 'Barbie'
+
     return (
-        <View>
-            <Text>Cast</Text>
+        <View style={{marginVertical: 6}}>
+            <Text style={{color: 'white', fontSize: 16, marginHorizontal: 4, marginBottom: 5}}>Top Cast</Text>
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{paddingHorizontal: 15}}
+            >
+                {
+                    cast && cast.map((person, index) => {
+                        return (
+                            <TouchableOpacity
+                                Key={index}
+                                style={{marginRight: 4, alignSelf: 'center'}}
+                                onPress={() => navigation.navigate('Person', person)}
+                            >
+                                <View
+                                    style={{
+                                        overflow: 'hidden',
+                                        borderRadius: '100%',
+                                        height: 50,
+                                        width: 50,
+                                        alignSelf: 'center'
+                                    }}
+                                >
+                                    <Image
+                                        source={require('../assets/margo.jpeg')}
+                                        style={{
+                                            borderRadius: 10,
+                                            height: 54,
+                                            width: 50 
+                                        }}
+                                    />
+                                </View>
+                                <Text style={{color: 'white', marginTop: 1, fontSize: 10}}>
+                                    {characterName.length>10 ? characterName.slice(0,10)+'...' : characterName}
+                                </Text>
+                                <Text style={{color: '#717573', marginTop: 1, fontSize: 10}}>
+                                    {personName.length>10 ? personName.slice(0,10)+'...' : personName}
+                                </Text>
+                            </TouchableOpacity>
+                        )
+                    })
+                }
+            </ScrollView>
         </View>
     )
 }
--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -81,7 +81,7 @@ const MovieScreen = () => {
                     During World War II, Lt. Gen. Leslie Groves Jr. appoints physicist J. Robert Oppenheimer to work on the top-secret Manhattan Project. Oppenheimer and a team of scientists spend years developing and designing the atomic bomb. Their work comes to fruition on July 16, 1945, as they witness the world's first nuclear explosion, forever changing the course of history.
                 </Text>
             </View>
-            <Cast cast={cast} />
+            <Cast cast={cast} navigation={navigation} />
         </ScrollView>
     )
 }
