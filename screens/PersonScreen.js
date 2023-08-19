5435a39a1ab6f90e4b3bd5e3344620a67668dd3b
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-19 15:39:15 -0400

    Person Screen added

---
+++ screens/PersonScreen.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { StyleSheet, View, Text } from "react-native"
+
+const PersonScreen = () => {
+    return (
+        <View>
+            <Text>Movie Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default PersonScreen
