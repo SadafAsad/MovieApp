e60e24e3e64953759642ee23cd49fd50750c012d
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-19 08:08:13 -0400

    Movie Poster added to Carousel

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -1,16 +1,30 @@
 import React from 'react'
-import { TouchableWithoutFeedback, StyleSheet, Text } from 'react-native'
+import { TouchableWithoutFeedback, StyleSheet, Text, Dimensions, Image } from 'react-native'
+
+var {width, height} = Dimensions.get('window')
 
 const MovieCard = ({ item }) => {
-    <TouchableWithoutFeedback>
-        <Text>Movie</Text>
-    </TouchableWithoutFeedback>
+    return (
+        <TouchableWithoutFeedback>
+            <Image 
+                source={require('../assets/barbie.jpg')} 
+                style={{
+                    width: width*0.6, 
+                    height: height*0.4, 
+                    borderRadius: 10
+                }}
+            />
+        </TouchableWithoutFeedback>
+    )
 }
 
 const styles = StyleSheet.create({
     text: {
         color: 'white',
         fontSize: 20
+    },
+    poster: {
+        
     }
 })
 
