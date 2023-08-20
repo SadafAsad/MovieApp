1f4cd211bff4ca5439872ae26d56670e5c90a231
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-20 13:05:29 -0400

    code clean up #2

--- screens/PersonScreen.js
+++ screens/PersonScreen.js
@@ -11,37 +11,23 @@ const ios = Platform.OS == 'ios'
 const verticalMargin = ios ? 0 : 3
 
 const PersonScreen = () => {
+    const { container, safeArea, backButton, shadow, circular, name, location, detail, detailContainer, detailTitle, detailText, bio, bioText } = styles
     const [isFavourite, toggleFavourite] = useState(false)
-    const navigation = useNavigation()
     const [personMovies, setPersonMovies] = useState([1, 2, 3, 4])
     const [loading, setLoading] = useState(false)
 
+    const navigation = useNavigation()
+
     return (
-        <ScrollView
-            style={{
-                flex: 1,
-                backgroundColor: '#262626',
-                paddingBottom: 20
-            }}
-        >
-            <SafeAreaView
-                style={{
-                    width: width,
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    paddingHorizontal: 4,
-                    zIndex: 20,
-                    marginVertical: {verticalMargin}
-                }}
-            >
+        <ScrollView style={container}>
+            <SafeAreaView style={safeArea}>
                 <TouchableOpacity
                     onPress={() => navigation.goBack()}
-                    style={{borderRadius: 10, padding: 1, backgroundColor: '#eab308'}}
+                    style={backButton}
                 >
                     <ChevronLeftIcon size='28' strokeWidth={2.5} color='white' />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
+                <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)} style={{marginRight: 5}}>
                     <HeartIcon size='35' color={isFavourite ? 'red' : 'white'} />
                 </TouchableOpacity>
             </SafeAreaView>
@@ -50,66 +36,40 @@ const PersonScreen = () => {
                     <Loading />
                 ) : (
                     <View>
-                        <View
-                            style={{
-                                flexDirection: 'row',
-                                justifyContent: 'center',
-                                shadowColor: 'gray',
-                                shadowRadius: 40,
-                                shadowOffset: {width: 0, height: 5},
-                                shadowOpacity: 1
-                            }}
-                        >
-                            <View style={{
-                                alignItems: 'center',
-                                borderRadius: '100%',
-                                overflow: 'hidden'
-                            }}>
+                        <View style={shadow}>
+                            <View style={circular}>
                                 <Image
                                     source={require('../assets/margo.jpeg')}
-                                    style={{height: height*0.43, width: width*0.74}}
+                                    style={{height: '100%', width: '100%'}}
                                 />
                             </View>
                         </View>
-                        <View style={{marginTop: 6}}>
-                            <Text style={{fontSize: 20, color: 'white', fontWeight: 'bold', alignSelf: 'center'}}>
-                                Margo Robbie
-                            </Text>
-                            <Text style={{fontSize: 16, color: '#717573', fontWeight: 'bold', alignSelf: 'center', margin: 4}}>
-                                London, United Kingdom
-                            </Text>
+                        <View style={{marginTop: 5}}>
+                            <Text style={name}>Margo Robbie</Text>
+                            <Text style={location}>London, United Kingdom</Text>
                         </View>
                         <View 
-                            style={{
-                                marginHorizontal: 3, 
-                                marginTop: 6, 
-                                flexDirection: 'row', 
-                                justifyContent: 'space-between', 
-                                alignItems: 'center',
-                                backgroundColor: '#959c99',
-                                borderRadius: '100%',
-                                padding: 4
-                            }}>
-                            <View style={{borderRightWidth: 2, paddingHorizontal: 2, alignItems: 'center', borderColor: '#717573'}}>
-                                <Text style={{color: 'white', fontWeight: 'bold'}}>Gender</Text>
-                                <Text style={{color: '#717573', fontSize: 15}}>Male</Text>
+                            style={detailContainer}>
+                            <View style={detail}>
+                                <Text style={detailTitle}>Gender</Text>
+                                <Text style={detailText}>Male</Text>
                             </View>
-                            <View style={{borderRightWidth: 2, paddingHorizontal: 2, alignItems: 'center', borderColor: '#717573'}}>
-                                <Text style={{color: 'white', fontWeight: 'bold'}}>Birthday</Text>
-                                <Text style={{color: '#717573', fontSize: 15}}>1964-09-09</Text>
+                            <View style={detail}>
+                                <Text style={detailTitle}>Birthday</Text>
+                                <Text style={detailText}>1964-09-09</Text>
                             </View>
-                            <View style={{borderRightWidth: 2, paddingHorizontal: 2, alignItems: 'center', borderColor: '#717573'}}>
-                                <Text style={{color: 'white', fontWeight: 'bold'}}>Known for</Text>
-                                <Text style={{color: '#717573', fontSize: 15}}>Acting</Text>
+                            <View style={detail}>
+                                <Text style={detailTitle}>Known for</Text>
+                                <Text style={detailText}>Acting</Text>
                             </View>
-                            <View style={{paddingHorizontal: 2, alignItems: 'center', borderColor: '#717573'}}>
-                                <Text style={{color: 'white', fontWeight: 'bold'}}>Popularity</Text>
-                                <Text style={{color: '#717573', fontSize: 15}}>64.62</Text>
+                            <View style={{paddingHorizontal: 5, alignItems: 'center'}}>
+                                <Text style={detailTitle}>Popularity</Text>
+                                <Text style={detailText}>64.62</Text>
                             </View>
                         </View>
-                        <View style={{marginVertical: 6, marginHorizontal: 4}}>
-                            <Text style={{color: 'white', fontSize: 20}}>Biography</Text>
-                            <Text style={{color: '#717573', letterSpacing: 1}}>Margot Elise Robbie is an Australian actress and producer. Known for her work in both blockbuster and independent films, she has received various accolades, including nominations for two Academy Awards, four Golden Globe Awards, and five British Academy Film Awards.</Text>
+                        <View style={{margin: 5}}>
+                            <Text style={bio}>Biography</Text>
+                            <Text style={bioText}>Margot Elise Robbie is an Australian actress and producer. Known for her work in both blockbuster and independent films, she has received various accolades, including nominations for two Academy Awards, four Golden Globe Awards, and five British Academy Film Awards.</Text>
                         </View>
                         <MovieList title={'Movies'} data={personMovies} hideSeeAll={true} />
                     </View>
@@ -120,7 +80,90 @@ const PersonScreen = () => {
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: '#262626',
+        paddingBottom: 20
+    },
+    safeArea: {
+        width: width,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 4,
+        zIndex: 20,
+        marginVertical: {verticalMargin}
+    },
+    backButton: {
+        borderRadius: 10, 
+        padding: 1, 
+        backgroundColor: '#eab308',
+        marginLeft: 5
+    },
+    shadow: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        shadowColor: 'gray',
+        shadowRadius: 40,
+        shadowOffset: {width: 0, height: 5},
+        shadowOpacity: 1
+    },
+    circular: {
+        alignItems: 'center',
+        width: 250,
+        height: 250,
+        borderRadius: 125,
+        overflow: 'hidden',
+        justifyContent: 'center',
+        borderColor: '#ccc', 
+        borderWidth: 2
+    },
+    name: {
+        fontSize: 20, 
+        color: 'white', 
+        fontWeight: 'bold', 
+        alignSelf: 'center'
+    },
+    location: {
+        fontSize: 15,
+        color: 'gray', 
+        fontWeight: 'bold', 
+        alignSelf: 'center', 
+        margin: 4
+    },
+    detailContainer: {
+        marginHorizontal: 5, 
+        marginTop: 5, 
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        alignItems: 'center',
+        backgroundColor: 'gray',
+        borderRadius: 50,
+        padding: 8
+    },
+    detail: {
+        borderRightWidth: 1, 
+        paddingHorizontal: 5, 
+        alignItems: 'center', 
+        borderColor: 'lightgray'
+    },
+    detailTitle: {
+        color: 'white', 
+        fontWeight: 'bold'
+    },
+    detailText: {
+        color: 'lightgray', 
+        fontSize: 15
+    },
+    bio: {
+        color: 'white', 
+        fontSize: 18
+    },
+    bioText: {
+        color: '#717573', 
+        letterSpacing: 1, 
+        marginTop: 5
+    }
 })
 
 export default PersonScreen
