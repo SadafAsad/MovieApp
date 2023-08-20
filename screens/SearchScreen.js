eec233e1853688a24a3917c0baf627b44a5d3823
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-20 09:01:46 -0400

    Search Screen partially done

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,9 +1,49 @@
 import React from "react"
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, View, SafeAreaView, Dimensions, TextInput, TouchableOpacity } from "react-native"
+import { XMarkIcon } from "react-native-heroicons/outline"
+import { useNavigation } from "@react-navigation/native"
+
+var {width, height} = Dimensions.get('window')
 
 const SearchScreen = () => {
+    const navigation = useNavigation()
+
     return (
-        <View></View>
+        <SafeAreaView style={{flex: 1, backgroundColor: '#262626'}}>
+            <View style={{
+                marginHorizontal: 4,
+                marginBottom: 3,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignContent: 'center',
+                borderRadius: '100%',
+                borderColor: '#717573',
+                borderWidth: 1
+            }}>
+                <TextInput
+                    placeholder="Search Movie"
+                    placeholderTextColor={'lightgray'}
+                    style={{
+                        paddingBottom: 1,
+                        paddingLeft: 6,
+                        flex: 1,
+                        fontWeight: 'bold',
+                        color: 'white',
+                        letterSpacing: 1
+                    }}
+                />
+                <TouchableOpacity onPress={() => navigation.goBack()}
+                    style={{
+                        borderRadius: '100%',
+                        padding: 3,
+                        margin: 1,
+                        backgroundColor: '#717573'
+                    }}
+                >
+                    <XMarkIcon size='25' color='white' />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     )
 }
 
