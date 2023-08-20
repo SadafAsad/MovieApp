90c24653e9dd3ccfec850c1fc324ccf99cab7c8b
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-20 09:18:45 -0400

    scroll view in search screen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { StyleSheet, View, SafeAreaView, Dimensions, TextInput, TouchableOpacity } from "react-native"
+import React, { useState } from "react"
+import { StyleSheet, View, SafeAreaView, Dimensions, TextInput, TouchableOpacity, ScrollView, Text, TouchableWithoutFeedback, Image } from "react-native"
 import { XMarkIcon } from "react-native-heroicons/outline"
 import { useNavigation } from "@react-navigation/native"
 
@@ -7,6 +7,8 @@ var {width, height} = Dimensions.get('window')
 
 const SearchScreen = () => {
     const navigation = useNavigation()
+    const [results, setResults] = useState([1, 2, 3, 4])
+    let movieName = 'Barbie'
 
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: '#262626'}}>
@@ -43,6 +45,39 @@ const SearchScreen = () => {
                     <XMarkIcon size='25' color='white' />
                 </TouchableOpacity>
             </View>
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{paddingHorizontal: 15}}
+                style={{marginBottom: 3}}
+            >
+                <Text style={{color: 'white', fontWeight: 'bold', marginLeft: 1}}>Results ({results.length})</Text>
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    flexWrap: 'wrap'
+                }}>
+                    {
+                        results.map((item, index) => {
+                            return(
+                                <TouchableWithoutFeedback
+                                    key={index}
+                                    onPress={() => navigation.push('Movie', item)}
+                                >
+                                    <View style={{marginBottom: 4, marginVertical: 2}}>
+                                        <Image
+                                            style={{borderRadius: 10, width: width*0.44, height: height*0.3}}
+                                            source={require('../assets/barbie.jpg')}
+                                        />
+                                        <Text style={{color: 'white', marginLeft: 1}}>
+                                            {movieName.length>22 ? movieName.slice(0,22)+'...' : movieName}
+                                        </Text>
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            )
+                        })
+                    }
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
