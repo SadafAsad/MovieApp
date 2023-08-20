3fdec8dd7f80fa73c5455df1b50bc0adef21606e
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-20 18:50:27 -0400

    PErson APIs added

--- api/moviedb.js
+++ api/moviedb.js
@@ -10,6 +10,9 @@ const movieDetailsEndpoint = id => `${apiBaseURL}/movie/${id}?api_key=${apiKey}`
 const movieCreditsEndpoint = id => `${apiBaseURL}/movie/${id}/credits?api_key=${apiKey}`
 const similarMoviesEndpoint = id => `${apiBaseURL}/movie/${id}/similar?api_key=${apiKey}`
 
+const personDetailsEndpoint = id => `${apiBaseURL}/person/${id}?api_key=${apiKey}`
+const personMoviesEndpoint = id => `${apiBaseURL}/person/${id}/movie_credits?api_key=${apiKey}`
+
 export const image500 = path => path ? `https://image.tmdb.org/t/p/w500${path}` : null
 export const image342 = path => path ? `https://image.tmdb.org/t/p/w342${path}` : null
 export const image185 = path => path ? `https://image.tmdb.org/t/p/w185${path}` : null
@@ -50,4 +53,11 @@ export const fetchMovieCredits = id => {
 }
 export const fetchSimilarMovies = id => {
     return apiCall(similarMoviesEndpoint(id))
+}
+
+export const fetchPersonDetails = id => {
+    return apiCall(personDetailsEndpoint(id))
+}
+export const fetchPersonMovies = id => {
+    return apiCall(personMoviesEndpoint(id))
 }
