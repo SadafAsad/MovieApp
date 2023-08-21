b279c7338d67fd636a32ebb324dafe619ab2d0c4
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-21 12:25:44 -0400

    Search Screen done

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,25 +1,47 @@
-import React, { useState } from "react"
+import React, { useCallback, useState } from "react"
 import { StyleSheet, View, SafeAreaView, Dimensions, TextInput, TouchableOpacity, ScrollView, Text, TouchableWithoutFeedback, Image } from "react-native"
 import { XMarkIcon } from "react-native-heroicons/outline"
 import { useNavigation } from "@react-navigation/native"
 import Loading from "../components/Loading"
+import { debounce } from 'lodash'
+import { fallBackMoviePoster, image185, searchMovie } from "../api/moviedb"
 
 var {width, height} = Dimensions.get('window')
 
 const SearchScreen = () => {
     const { searchArea, searchInput, cancel } = styles
 
-    const [results, setResults] = useState([1, 2, 3, 4, 5])
+    const [results, setResults] = useState([])
     const [loading, setLoading] = useState(false)
     
     const navigation = useNavigation()
     
-    let movieName = 'Barbie'
+    const handleSearch = value => {
+        if (value && value.length>2){
+            setLoading(true)
+            searchMovie({
+                query: value,
+                include_adult: 'false',
+                language: 'en-US',
+                page: '1'
+            }).then(data => {
+                setLoading(false)
+                if (data && data.results) setResults(data.results)
+            })
+        } else {
+            setLoading(false)
+            setResults([])
+        }
+    }
+    const handleTextDebounce = useCallback(debounce(handleSearch, 400), [])
+
+    let movieName = "Barbie"
 
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: '#262626'}}>
             <View style={searchArea}>
                 <TextInput
+                    onChangeText={handleTextDebounce}
                     placeholder="Search Movie"
                     placeholderTextColor={'gray'}
                     style={searchInput}
@@ -53,10 +75,11 @@ const SearchScreen = () => {
                                                 <View style={{marginVertical: 5}}>
                                                     <Image
                                                         style={{borderRadius: 10, width: width*0.44, height: height*0.3}}
-                                                        source={require('../assets/barbie.jpg')}
+                                                        // source={require('../assets/barbie.jpg')}
+                                                        source={{uri: image185(item?.poster_path) || fallBackMoviePoster}}
                                                     />
                                                     <Text style={{color: 'white', marginLeft: 5, marginTop: 5, color: 'lightgray'}}>
-                                                        {movieName.length>22 ? movieName.slice(0,22)+'...' : movieName}
+                                                        {item?.title.length>22 ? item?.title.slice(0,22)+'...' : item?.title}
                                                     </Text>
                                                 </View>
                                             </TouchableWithoutFeedback>
