bb7446dc7683c0cf5005f0e2961dfab480284a9c
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-23 10:59:00 -0400

    movie list ui u[date

--- components/MovieList.js
+++ components/MovieList.js
@@ -2,6 +2,7 @@ import React from "react"
 import { View, StyleSheet, Text, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Image, Dimensions } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import { fallBackMoviePoster, image185 } from "../api/moviedb"
+import { PlusIcon } from "react-native-heroicons/outline"
 
 var {width, height} = Dimensions.get('window')
 
@@ -20,33 +21,51 @@ const MovieList = ({ title, data, hideSeeAll }) => {
                     )
                 }
             </View>
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{paddingHorizontal: 15}}
-            >
-                {
-                    data.map((item, index) => {
-                        return (
-                            <TouchableWithoutFeedback
-                                key={index}
-                                onPress={() => navigation.push('Movie', item)}
-                            >
-                                <View style={{margin: 4}}>
-                                    <Image 
-                                        // source={require('../assets/oppenheimer.jpg')}
-                                        source={{uri: image185(item?.poster_path) || fallBackMoviePoster}}
-                                        style={{width: width*0.33, height: height*0.22, borderRadius: 10}}
-                                    />
-                                    <Text style={{color: 'white', marginTop: 5}}>
-                                        {item?.title?.length>14 ? item?.title?.slice(0,14)+'...' : item?.title}
-                                    </Text>
-                                </View>
-                            </TouchableWithoutFeedback>
-                        )
-                    })
-                }
-            </ScrollView>
+            {
+                data.length>0 ? (
+                    <ScrollView
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        contentContainerStyle={{paddingHorizontal: 15}}
+                    >
+                        {
+                            data.map((item, index) => {
+                                return (
+                                    <TouchableWithoutFeedback
+                                        key={index}
+                                        onPress={() => navigation.push('Movie', item)}
+                                    >
+                                        <View style={{margin: 4}}>
+                                            <Image 
+                                                // source={require('../assets/oppenheimer.jpg')}
+                                                source={{uri: image185(item?.poster_path) || fallBackMoviePoster}}
+                                                style={{width: width*0.33, height: height*0.22, borderRadius: 10}}
+                                            />
+                                            <Text style={{color: 'white', marginTop: 5}}>
+                                                {item?.title?.length>14 ? item?.title?.slice(0,14)+'...' : item?.title}
+                                            </Text>
+                                        </View>
+                                    </TouchableWithoutFeedback>
+                                )
+                            })
+                        }
+                    </ScrollView>
+                ) : (
+                    <View style={{
+                        backgroundColor: '#262626', 
+                        width: width, 
+                        height: height*0.22,
+                        justifyContent: 'center'
+                    }}>
+                        <TouchableOpacity 
+                            style={{justifyContent: 'center', alignItems: 'center'}}
+                        >
+                            <PlusIcon size={50} strokeWidth={2} color={'white'}/>
+                            <Text style={{color: 'white'}}>Add movies to your list</Text>
+                        </TouchableOpacity>
+                    </View>
+                )
+            }
         </View>
     )
 }
